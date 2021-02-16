0a1b8bf08cdfbc8c846830a385dba71256c89597
PIPIFLOR\pipiflor <margri764@gamil.com>
2021-02-16 10:08:56 -0300

    no message

--- src/app/servicios/servarrayfotos.service.ts
+++ src/app/servicios/servarrayfotos.service.ts
@@ -13,82 +13,47 @@ export class ServarrayfotosService {
 {
   id:0,
   nombre: "Set Matero",
-  img: "assets/stencil2.jpg",
+  img: "assets/compu.jpg",
   precio:"250"
 },
 {
   id:1,
   nombre: "Masitero c/vidrio",
-  img: "assets/camara1.jpg",
+  img: "assets/compu.jpg",
   precio:"170"
 },
 {
   id:10,
   nombre: "Fondo moderno",
-  img: "assets/fondo.jpg",
+  img: "assets/compu.jpg",
   precio:"1700"
 },
 {
   id:2,
   nombre: "Cajon grande",
-  img: "assets/camara2.jpg",
+  img: "assets/compu.jpg",
   precio:"130"
   },
   {
     id:3,
     nombre: "Cajon frente",
-    img: "assets/camara3.jpg",
+    img: "assets/compu.jpg",
     precio:"200"
     },
 
     {
       id:4,
       nombre: "Set Matero2",
-      img: "assets/stencil2.jpg",
+      img: "assets/compu.jpg",
       precio:"250"
     },  
     {  
     id:5,
     nombre: "Cajon frente2",
-    img: "assets/camara3.jpg",
+    img: "assets/compu.jpg",
     precio:"200"
     },
-    {
-      id:6,
-      nombre: "Set Matero3",
-      img: "assets/stencil2.jpg",
-      precio:"250"
-    },  
-    {
-      id:7,
-      nombre: "Masitero c/vidrio2",
-      img: "assets/camara1.jpg",
-      precio:"170"
-    },
-    {
-      id:8,
-      nombre: "Set Matero",
-      img: "assets/stencil2.jpg",
-      precio:"250"
-    },
-    {
-      id:9,
-      nombre: "Masitero c/vidrio",
-      img: "assets/camara1.jpg",
-      precio:"170"
-    },
-    {
-      id:10,
-      nombre: "Cajon grande",
-      img: "assets/camara2.jpg",
-      precio:"130"
-      },
-      {
-        id:11,
-        nombre: "Cajon frente",
-        img: "assets/camara3.jpg",
-        precio:"200"
-        },
+    
     
 ];
 
