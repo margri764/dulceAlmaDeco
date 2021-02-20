3ba9d91cd7fefa4673edf26c29982738cedc5cb4
PIPIFLOR\pipiflor <margri764@gamil.com>
2021-02-20 15:24:13 -0300

    no message

--- src/app/componentes/ubicacion/ubicacion.component.ts
+++ src/app/componentes/ubicacion/ubicacion.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UbicacionComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+
+
+
+    
+  }
+  
 
   ngOnInit(): void {
   }
